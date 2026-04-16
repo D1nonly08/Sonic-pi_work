36aa05c93146e8cda89f604580e2e18016c9c9e1
D1nonly08 <diego_gomez@student.davincischools.org>
2026-04-16 11:24:27 -0700

    Update sonic.pi_2.rb

--- sonic.pi_2.rb
+++ sonic.pi_2.rb
@@ -3,37 +3,41 @@ intro = "C:/Users/diego_gomez/Downloads/Justice - Incognito (Official Audio).wav
 twice = "C:/Users/diego_gomez/Downloads/incognito.wav"
 secondsample = "C:/Users/diego_gomez/Downloads/secondsample.wav"
 
-sample intro
-
-sleep 42
-
 live_loop :backround do
   sample :loop_drone_g_97, beat_stretch: 16, amp: 2, pitch_stretch: 3
   sleep 5.5
 end
 
+sleep 2
+sample intro, amp: 2.5
+
+sleep 38
+
+sample :ambi_haunted_hum, amp: 2, beat_stretch: 3
+sleep 3
+
 live_loop :background do
   3.times do
-    sample twice
-    sleep 3.5
+    sample twice, amp: 0.7
+    sleep 3
   end
-  sample secondsample
-  sleep 3.5
+  sample secondsample, amp: 0.7
+  sleep 3
 end
 
-sleep 14
+sleep 10.5
 
 live_loop :tuff do
   use_synth :prophet
-  play 60, amp: 0.5
-  sleep 0.3
-  play 70, amp: 0.5
-  sleep 0.3
+  play 60, amp: 1
+  sleep 0.32
+  play 70, amp: 1
+  sleep 0.32
 end
 
 live_loop :drums do
-  sample :drum_cymbal_hard, amp: 0.7
-  sleep 0.3
-  sample :drum_bass_hard, amp: 0.7
-  sleep 0.3
+  sample :drum_cymbal_hard, amp: 1.2
+  sleep 0.32
+  sample :drum_bass_hard, amp: 1.2
+  sleep 0.32
 end
